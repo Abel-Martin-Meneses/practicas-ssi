1e2af73cd4fa1e7157e869f91863bb3fb8882487
Abel Martín Meneses <alu0101660011@ull.edu.es>
2026-04-08 20:50:18 +0100

    Práctica 9 BB84 Terminada?

---
+++ PRACTICA_9/PRACTICA_9_BB84_ORIGINAL/main.py
@@ -0,0 +1,75 @@
+import numpy as np
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+
+# ==========================================
+# 1.1 Librerías y variables iniciales
+# ==========================================
+n = 10 ** 3
+qubit_index = 0
+backend = AerSimulator()
+message = [QuantumCircuit(1, 2) for _ in range(n)]
+
+# ==========================================
+# 1.2 Preparación de estados (Alice)
+# ==========================================
+alice_bits = np.random.randint(2, size=n)
+alice_bases = np.random.randint(2, size=n)
+
+for i, qc in enumerate(message):
+    if alice_bits[i] == 1:
+        qc.x(0)
+    if alice_bases[i] == 1:
+        qc.h(0)
+
+# ==========================================
+# 1.3 Cambio de bases (Bob)
+# ==========================================
+bob_bases = np.random.randint(2, size=n)
+
+for i, qc in enumerate(message):
+    if bob_bases[i] == 1:
+        qc.h(0)
+
+# ==========================================
+# 1.4 Medida
+# ==========================================
+classical_bit_index_for_bob_measurement = 0
+
+for qc in message:
+    qc.measure(qubit_index, classical_bit_index_for_bob_measurement)
+
+# ==========================================
+# 1.5 Comparación de bases
+# ==========================================
+cbit = classical_bit_index_for_bob_measurement
+bob_measured_bits = []
+
+for qc in message:
+    transpiled_qc = transpile(qc, backend)
+    result = backend.run(transpiled_qc, shots=1).result()
+    counts = result.get_counts()
+    measured_bit = list(counts.keys())[0][::-1][cbit]
+    bob_measured_bits.append(measured_bit)
+
+alice_key = []
+bob_key = []
+
+for i in range(n):
+    if alice_bases[i] == bob_bases[i]:
+        alice_key.append(int(alice_bits[i]))
+        bob_key.append(int(bob_measured_bits[i]))
+
+# ==========================================
+# 1.6 Cálculo del QBER
+# ==========================================
+errores = 0
+for i in range(len(alice_key)):
+    if alice_key[i] != bob_key[i]:
+        errores += 1
+
+qber = errores / len(alice_key) if len(alice_key) > 0 else 0
+
+print(f"Longitud original (n): {n} bits")
+print(f"Longitud de la clave final coincidente: {len(alice_key)} bits")
+print(f"Tasa de error cuántico (QBER): {qber}")
